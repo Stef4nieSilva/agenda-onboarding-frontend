14fa44adc32f6a3dfd35858e7da3dbd361b1959e
Stefanie Silva <stef4nie.tms@gmail.com>
2025-11-24 18:11:03 -0300

    atualizações

--- src/Agenda.jsx
+++ src/Agenda.jsx
@@ -180,7 +180,7 @@ agendamentos.forEach((a) => {
   return Array.from(base).sort((a, b) => a.localeCompare(b));
 };
 
-const todosAgentesOrdenados = ["Anny", "Diego", "Stefanie", "Vanessa"];
+const todosAgentesOrdenados = ["Diego", "Maria", "Priscila", "Maria"];
 const agentesFiltrados =
   agenteSelecionado === "Todos" ? todosAgentesOrdenados : [agenteSelecionado];
 
